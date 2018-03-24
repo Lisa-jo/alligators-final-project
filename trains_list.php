c3251c95b486c06c4e9b458670c01597b3530bb6
Lisa-jo <37560178+Lisa-jo@users.noreply.github.com>
2018-03-24 16:06:24 -0500

    Add files via upload
    Updated PHP files to fix icons and search bar. New image for search bar. Image should go in images folder.

--- trains_list.php
+++ trains_list.php
@@ -9,7 +9,7 @@
 <div class="right-content">
     <div class="container">
 
-        <h3><img src="images/image16.png" style="max-height: 35px;" /> Solution Trains (ST), <img src="images/image17.png" style="max-height: 35px;" />Agile Release Trains (ART), <img src="images/image20.png" style="max-height: 35px;" />Agile Teams (AT)</h3>
+        <h3><img src="images/solution_train.png" style="max-height: 35px;" /> Solution Trains (ST), <img src="images/image17.png" style="max-height: 35px;" />Agile Release Trains (ART), <img src="images/image20.png" style="max-height: 35px;" />Agile Teams (AT)</h3>
 
         <table id="info" cellpadding="0" cellspacing="0" border="0" class="datatable table table-striped table-bordered datatable-style"
                width="100%" style="width: 100px;">
